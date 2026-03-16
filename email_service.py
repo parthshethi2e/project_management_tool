3b0f38398d9b66d93ef341ecceef799b1268de6c
parthshethi2e <126868452+parthshethi2e@users.noreply.github.com>
2026-03-16 17:37:27 +0530

    Fixes

--- email_service.py
+++ email_service.py
@@ -1,14 +1,14 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+import os
 
 def send_email(to_email, subject, html_body):
 
     msg = MIMEMultipart("alternative")
 
     msg["Subject"] = subject
-    msg["From"] = "prafullsakpali2e@gmail.com"
+    msg["From"] = os.getenv("EMAIL_USER")
     msg["To"] = to_email
 
     html_part = MIMEText(html_body, "html")
@@ -18,8 +18,13 @@ def send_email(to_email, subject, html_body):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
 
+    sender_email = os.getenv("EMAIL_USER")
+    password = os.getenv("EMAIL_PASSWORD")
 
-    server.login("prafullsakpali2e@gmail.com", "bagd ylkm gcts hnmi")
+
+    server.login(sender_email, password)
+    
+    
 
     server.sendmail(msg["From"], [msg["To"]], msg.as_string())
 
